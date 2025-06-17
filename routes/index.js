c8a80aaa878d567806409e89f9a171fa78bee435
Maximillionair <maxmillian.heim@outlook.com>
2025-06-17 13:07:48 +0200

    removed is authenticated

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const superheroController = require('../controllers/superheroController');
-const { isAuthenticated } = require('../middleware/auth');
+// const { isAuthenticated } = require('../middleware/auth');
 
 
 // Home page route
-router.get('/' ,isAuthenticated, superheroController.getIndex);
+router.get('/', superheroController.getIndex);
 
 module.exports = router;
